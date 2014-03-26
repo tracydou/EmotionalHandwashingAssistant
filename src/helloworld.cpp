c444dc940480adc0b9db03d73755563f133236d7
l39lin <l39lin@uwaterloo.ca>
2014-03-26 01:38:53 -0400

    helloworld

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -2,25 +2,27 @@
 
 #include <iostream>
 #include <stdlib.h>
+
+#include "sample_client_server.cpp"
 using namespace std;
 
 int main() {
   cout << "Hello World for learning zeromq!" << endl;
-  
+  Server server;
+  Client client;
   int pid=fork();
   if (pid < 0 ) { // failed to fork
     cout << "failed to fork child process!" << endl;  
     return -1;
   } else if (pid == 0) { // child process
     // change directory and start the BayesactEngine server
-    chdir("../lib/BayesactEngine");
-    system("python ./bayesactinteractive.py");
+    // chdir("../lib/BayesactEngine");
+    // system("python ./bayesactinteractive.py");
+    server.Start();
   } else { // parent process
     // change back to directory
     // and connect the BayesActClient client & server
-    while(true) {
-	  cout << "done!" << endl;
-    }
-    return 0;
+    client.Start();
   }
+  return 0;
 }
