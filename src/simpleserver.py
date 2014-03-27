682f3bd6ccc88aa3d17b22422ba742e445d3dfa9
tracydou <tracydou@users.noreply.github.com>
2014-03-27 03:33:19 -0700

    Update simpleserver.py
    add protobuf logic, untested!

--- src/simpleserver.py
+++ src/simpleserver.py
@@ -1,5 +1,6 @@
 import time
 import zmq
+import BayesActMessage_pb2
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
@@ -8,10 +9,19 @@ socket.bind("tcp://*:5555")
 while True:
     #  Wait for next request from client
     message = socket.recv()
-    print("Received request: %s" % message)
+
+    request = BayesActMessage_pb2.BayesActRequest()
+    request.ParseFromString(message)
+    
+    print("Received request: %s" % request.__str__())
 
     #  Do some 'work'
     time.sleep(1)
+    
+    response = BayesActMessage_pb2.BayesActResponse()
+    response.evaluation = request.evaluation
+    response.potency = request.potency
+    response.activity = request.activity
 
     #  Send reply back to client
-    socket.send(b"World")
+    socket.send(response.SerializeToString())
