f4b2a4e4ba8214173632a1d4c0ee3af456da5841
l39lin <l39lin@scspc413.cs.uwaterloo.ca>
2014-04-07 17:14:09 -0400

    play around with "handtracker"

--- lib/Grabber_TrackingModel/tracker.hpp
+++ lib/Grabber_TrackingModel/tracker.hpp
@@ -5,7 +5,7 @@
  *  Last Modified:		April 2014
  *  Last Modified by:	Luyuan Lin
  *  
- *  This file and main.cpp are all modified from the old main.cpp
+ *  This file and tracker.cpp are both modified from the old main.cpp
  *  file. Changed places include newly added funcs and several changes
  *  to original main(), mainIdle() (now trackerIdle()), etc.
  * 
@@ -42,20 +42,45 @@
 #include "grab.h"
 #include "kalmanfilter.h"
 
+// ZMQ & others, used in class ZMQServer
+#include <string>
+#include <vector>
+#include <zmq.h>
+#include "../../cppzmq/zmq.hpp"
+#include "tracker_message.pb.h"
+
+using std::string;
+using std::vector;
+
 #define BILLION  1000000000L;
 
 // converted from mainIdle() by changing func name
 gint trackerIdle(void* data);
 
 // listen to requests from "client" and process recordingly
-gint processRequestsIdle(void* data);
+gint processRequestsIdle(TrackerServerStub* server_stub);
 // 1) if requests for LeftHandPos
-void respondWithLeftHandPos();
+bool respondWithLeftHandPos(TrackerServerStub* server_stub);
 // 2) else if requests for RightHandPos
-void respondWithRightHandPos();
+bool respondWithRightHandPos(TrackerServerStub* server_stub);
 // 3) else if requests for HandAction
-void respondWithHandAction();
+bool respondWithAction(TrackerServerStub* server_stub);
 
-// connect to server
+public class TrackerServerStub {
+public:
+    TrackerServerStub(string addr);
+    ~TrackerServerStub();
+    int Receive();
+    bool SendHandPos(Point3_<float> hand_position);
+    bool SendAction(int action);
+    
+    const static int TYPE_ERROR = -1;
+    const static int TYPE_LEFT_HAND_POS = 0;
+    const static int TYPE_RIGHT_HAND_POS = 1;
+    const static int TYPE_ACTION = 2;
+private:    
+    zmq::context_t context;
+    zmq::socket_t socket;
+};
 
 #endif  // TRACKER_
