25fc8cef91b90c74668ba6a9419092b74e6c9ecc
l39lin <tracydou@hotmail.com>
2014-05-23 16:37:56 -0400

    updated reconPsDict. Note that the dictionary gives prompt number (not behaviour number) for a particular planstep

--- lib/bayesact/assistant.py
+++ lib/bayesact/assistant.py
@@ -33,8 +33,7 @@ class Assistant(Agent):
         self.num_plansteps=len(self.nextPsDict)
         self.num_behaviours=len(self.nextBehDict)
 
-        self.reconPsDict = {0:2,1:3,2:3,3:4,4:5,5:7,6:7,7:7}
-        #self.reconPsDict = {0:1,1:2,2:3,3:4,4:5,5:6,6:7,7:7}
+        self.reconPsDict = {0:1,1:2,2:3,3:4,4:5,5:7,6:7,7:7}
 
 
         #initial awarness distribution
